13087068cb9e8389a497ab2610175515ec984de8
Rehnell Blackett <149915517+Rehnell@users.noreply.github.com>
2024-11-13 17:39:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ function getWeather() {
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
+// Response and alert message 
+
     fetch(currentWeatherUrl)
         .then(response => response.json())
         .then(data => {
